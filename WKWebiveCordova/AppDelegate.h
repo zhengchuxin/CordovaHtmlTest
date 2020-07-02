71cbe1c11512bea318a8e1bfdaabb4a1f8a80833
zcx <931140318@qq.com>
2020-07-02 14:50:36 +0800

    Initial Commit

---
+++ WKWebiveCordova/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WKWebiveCordova
+//
+//  Created by apple on 2020/7/2.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WKWebiveCordova/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebiveCordova
+//
+//  Created by apple on 2020/7/2.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
